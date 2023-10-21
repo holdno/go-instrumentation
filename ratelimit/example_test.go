dfe286d18ca04b8aea33bad994028b813eb5eb97
wby <wby@ojbk.io>
2023-10-21 16:12:16 +0800

    save

--- ratelimit/example_test.go
+++ ratelimit/example_test.go
@@ -3,11 +3,29 @@ package ratelimit_test
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 
 	"github.com/holdno/go-instrumentation/ratelimit"
 )
+
+func ExampleNew() {
+	ctx := context.Background()
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	_ = rdb.FlushDB(ctx).Err()
+	limit := ratelimit.New("project:123", 10, time.Second*10, ratelimit.NewLimiter(rdb, ratelimit.KeyPrefix("yourprefix_")))
+
+	fmt.Println(limit.Allow())
+
+	fmt.Println(limit.AllowN(10))
+
+	// Output:
+	// true
+	// true
+}
 
 func ExampleNewLimiter() {
 	ctx := context.Background()
@@ -17,10 +35,28 @@ func ExampleNewLimiter() {
 	_ = rdb.FlushDB(ctx).Err()
 
 	limiter := ratelimit.NewLimiter(rdb, ratelimit.KeyPrefix("yourprefix_"))
-	res, err := limiter.Allow(ctx, "project:123", ratelimit.PerSecond(10))
+
+	res, err := limiter.Allow(ctx, "project:123", ratelimit.Limit{
+		Rate:   1,
+		Burst:  1,
+		Period: time.Second * 10,
+	})
 	if err != nil {
 		panic(err)
 	}
+
+	time.Sleep(time.Second * 10)
+
+	res, err = limiter.Allow(ctx, "project:123", ratelimit.Limit{
+		Rate:   10,
+		Burst:  10,
+		Period: time.Second * 10,
+	})
+	if err != nil {
+		panic(err)
+	}
+
 	fmt.Println("allowed", res.Allowed, "remaining", res.Remaining)
+
 	// Output: allowed 1 remaining 9
 }
--- ratelimit/local.go
+++ ratelimit/local.go
@@ -1,5 +1,67 @@
 package ratelimit
 
-func NewWithLocal() {
-	
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+type rateLimiterWithLocalImpl struct {
+	local         *rate.Limiter
+	remote        *Limiter
+	remoteLimiter Limit
+	limit         int
+	period        time.Duration
+	key           string
+}
+
+func New(key string, limit int, period time.Duration, remoteLimiter *Limiter) RateLimiter {
+	impl := &rateLimiterWithLocalImpl{
+		local:  rate.NewLimiter(rate.Every(period), limit),
+		remote: remoteLimiter,
+		remoteLimiter: Limit{
+			Period: period,
+			Rate:   limit,
+			Burst:  limit,
+		},
+		limit:  limit,
+		period: period,
+		key:    key,
+	}
+	return impl
+}
+
+type RateLimiter interface {
+	Allow() bool
+	AllowN(int) bool
+}
+
+func (r *rateLimiterWithLocalImpl) Allow() bool {
+	if r.remote != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+		defer cancel()
+		if res, err := r.remote.Allow(ctx, r.key, r.remoteLimiter); err == nil {
+			return res.Allowed == 1
+		} else {
+			fmt.Println(err.Error(), "downgrade")
+		}
+	}
+
+	return r.local.Allow()
+}
+
+func (r *rateLimiterWithLocalImpl) AllowN(n int) bool {
+	if r.remote != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+		defer cancel()
+		if res, err := r.remote.AllowN(ctx, r.key, r.remoteLimiter, n); err == nil {
+			return res.Allowed == 1
+		} else {
+			fmt.Println(err.Error(), "downgrade")
+		}
+	}
+
+	return r.local.AllowN(time.Now(), n)
 }
